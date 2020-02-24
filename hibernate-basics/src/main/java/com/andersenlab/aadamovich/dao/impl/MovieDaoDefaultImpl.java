136c9ab4700abadb73fb5961b4f753f03327582a
Alexey Adamovich <alex.adamovich83@gmail.com>
2020-02-24 16:55:14 +0300

    Delete commented jdbc methods in MovieDaoDefaultImpl

--- hibernate-basics/src/main/java/com/andersenlab/aadamovich/dao/impl/MovieDaoDefaultImpl.java
+++ hibernate-basics/src/main/java/com/andersenlab/aadamovich/dao/impl/MovieDaoDefaultImpl.java
@@ -25,30 +25,6 @@ public class MovieDaoDefaultImpl implements BaseMovieDao<Movie> {
         return localInstance;
     }
 
-//    @Override
-//    public Integer add(Movie movie) {
-//        int primaryKey = -1;
-//        try (final Connection connection = mysql.getConnection();
-//             final PreparedStatement state = connection.prepareStatement
-//                     ("INSERT INTO movie (name, genre, is_cool) VALUES (? , ? , ?)",
-//                             Statement.RETURN_GENERATED_KEYS)) {
-//            state.setString(1, movie.getName());
-//            state.setString(2, movie.getGenre());
-//            state.setBoolean(3, movie.isCool());
-//            final int rowsUpdated = state.executeUpdate();
-//            if (rowsUpdated > 0) {
-//                try (final ResultSet resultSet = state.getGeneratedKeys()) {
-//                    resultSet.next();
-//                    primaryKey = resultSet.getInt(1);
-//                }
-//            }
-//            return primaryKey;
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Fail to save movie to DB", e);
-//        }
-//    }
-
-
     @Override
     public Integer add(Movie m) {
         try (final Session session = HibernateUtil.getSession()) {
@@ -59,51 +35,12 @@ public class MovieDaoDefaultImpl implements BaseMovieDao<Movie> {
         }
     }
 
-//    @Override
-//    public Movie findById(Integer id) {
-//        try (final Connection connection = mysql.getConnection();
-//             final PreparedStatement state = connection.prepareStatement
-//                     ("SELECT * FROM movie WHERE id = ?")) {
-//            state.setInt(1, id);
-//            try (final ResultSet resultSet = state.executeQuery()) {
-//                final boolean exist = resultSet.next();
-//                if (!exist) {
-//                    return null;
-//                }
-//                final String name = resultSet.getString("name");
-//                final String genre = resultSet.getString("genre");
-//                final boolean isCool = resultSet.getBoolean("is_cool");
-//                final Movie movie = new Movie(name, genre, isCool);
-//                movie.setId(id);
-//                return movie;
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Fail to get movie from DB", e);
-//        }
-//    }
-
-
     @Override
     public Movie findById(Integer id) {
         try (final Session session = HibernateUtil.getSession()) {
             return session.get(Movie.class, id);
         }
     }
-
-//    @Override
-//    public boolean update(Movie movie) {
-//        try (final Connection connection = mysql.getConnection();
-//             final PreparedStatement state = connection.prepareStatement
-//                     ("UPDATE movie SET name = ?, genre = ?, is_cool = ? WHERE id = ?")) {
-//            state.setString(1, movie.getName());
-//            state.setString(2, movie.getGenre());
-//            state.setBoolean(3, movie.isCool());
-//            state.setInt(4, movie.getId());
-//            return state.executeUpdate() > 0;
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Fail to update movie in DB", e);
-//        }
-//    }
 
     /*
     Do not use session.update(m); as want this method to return some value
@@ -136,16 +73,4 @@ public class MovieDaoDefaultImpl implements BaseMovieDao<Movie> {
             return rowsDeleted > 0;
         }
     }
-
-    //    @Override
-//    public boolean delete(Integer id) {
-//        try (final Connection connection = mysql.getConnection();
-//             final PreparedStatement state = connection.prepareStatement
-//                     ("DELETE FROM movie WHERE id = ?")) {
-//            state.setInt(1, id);
-//            return state.executeUpdate() > 0;
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Fail to delete movie from DB", e);
-//        }
-//    }
 }
